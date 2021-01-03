1b4c114126b62c456d9a3a185020fd6a85ca3247
toainn92 <69770689+toainn92@users.noreply.github.com>
2021-01-03 15:10:25 -0800

    Add files via upload

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,45 @@
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  //get the coordinates of each city by adding city.location in the L.marker() function
+  //then add each location to the map with the addTo() function and pass themap object as the argument.
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: "#ff8c00",
+    fillColor: '#ffa500',
+    lineWeight: 4
+  })
+  //add data from each object in the cities array, use Leaflet's bindPopup() method on the marker() function
+  //format the population with a thousands separator, use .toLocaleString()
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
