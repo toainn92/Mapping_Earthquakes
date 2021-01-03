5fa6d743a701c02ca0b0637a4b25f6493768c565
toainn92 <69770689+toainn92@users.noreply.github.com>
2021-01-03 13:53:08 -0800

    update

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,6 +1,3 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
   center: [
