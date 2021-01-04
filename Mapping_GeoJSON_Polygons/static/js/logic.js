0c885b87acdebe3b1d81a939169196446849de0b
toainn92 <69770689+toainn92@users.noreply.github.com>
2021-01-03 16:00:24 -0800

    Add files via upload

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,57 @@
+// We create the street view tile layer that will be the default background of our map.
+// use the Mapbox Styles API
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+// Use the Mapbox Styles API
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+// control.layers from leaflet
+// baseMaps is the base layer object which will allow the two different map styles to be show on the index.html file.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let torontoHoods = "https://raw.githubusercontent.com/toainn92/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Create a style for the lines.
+let styleLine = {
+    color: "#0000FF",
+    weight: 1,
+    fillColor: "#ffffa1"
+  }
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+      style: styleLine,
+      onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h3>Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+      }
+    }).addTo(map);
+  });
+  
